dc990c45ee571585d2e56748ca7f92fca743a233
JMeils <77118044+JMeils@users.noreply.github.com>
2023-06-05 09:36:19 -0600

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,13 +17,13 @@ stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/maste
 # Row A
 a1, a2, a3 = st.columns(3)
 a1.image(Image.open('streamlit-logo-secondary-colormark-darktext.png'))
-a2.metric("Wind", "9 mph", "-8%")
+a2.metric("Windyyyy", "9 mph", "-8%")
 a3.metric("Humidity", "86%", "4%")
 
 # Row B
 b1, b2, b3, b4 = st.columns(4)
 b1.metric("Temperature", "70 °F", "1.2 °F")
-b2.metric("Wind", "9 mph", "-8%")
+b2.metric("Windyyyy", "9 mph", "-8%")
 b3.metric("Humidity", "86%", "4%")
 b4.metric("Humidity", "86%", "4%")
 
